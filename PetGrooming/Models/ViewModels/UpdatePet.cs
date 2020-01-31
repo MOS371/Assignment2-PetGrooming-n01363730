7209d1d51e0bbcd88f4e56bc9cb533b2d2fc7fd7
DELL <DELL@DESKTOP-4J357LQ>
2020-01-30 20:51:09 -0500

    Initial Commit

---
+++ PetGrooming/Models/ViewModels/UpdatePet.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PetGrooming.Models.ViewModels
+{
+    public class UpdatePet
+    {
+        //Info needed for updatng the pet work
+        //Info about one pet
+        //Info About many species
+        public Pet pet { get; set; }
+        public List<Species> species { get; set; }
+    }
+}
